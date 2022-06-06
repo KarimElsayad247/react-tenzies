89337829ba6cb9d21c89a3d2ee341699afc107a4
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-06-07 00:41:58 +0200

    Create die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 
+import Die from './Die'
+
 function App() {
+    const dies = []
+    for (let i = 0; i < 10; i++) {
+        dies.push(<Die number={i % 6 + 1}/>)
+    }
     return (
         <main>
-            
+            <div className="die-container">
+                {dies}
+            </div>  
         </main>
     )
 }
